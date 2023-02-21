cca540314ec8476fd7c361fcbedda39c426500b2
jinyeong03 <jinyeong2808@naver.com>
2023-02-21 14:37:18 +0900

    2.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import firebase from "firebase/compat/app";
-console.log(firebase)
+// console.log(firebase)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
