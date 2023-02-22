d76fb71333d60c5cdd4d381f6633d873ab2a26d7
jinyeong03 <jinyeong2808@naver.com>
2023-02-22 16:20:16 +0900

    3.2

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,23 @@
 import { DbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 export default function Home(){
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getJweets = async ()=>{
+        const dbJweets = await DbService.collection("jweets").get();
+        dbJweets.forEach((document)=>{
+            const jweetObject = {
+                ...document.data(),
+                id: document.id,
+            }
+            setNweets((prev) => [jweetObject, ...prev])
+        })
+    }   
+    useEffect(()=>{
+        getJweets();
+    },[])
     const onSubmit = async (event)=>{
         event.preventDefault();
         await DbService.collection("jweets").add({
@@ -22,6 +36,13 @@ export default function Home(){
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}></input>
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>
+                ))}
+            </div>
         </div>
     )
 }
