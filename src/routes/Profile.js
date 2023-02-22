f64d83ad04f127169569e6d7c24ead91af2f57bb
jinyeong03 <jinyeong2808@naver.com>
2023-02-22 11:50:39 +0900

    2.6

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,17 @@
+import { AuthService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function Profile(){
+    const history = useHistory();
+    const onLogOutClick = ()=>{
+        AuthService.signOut();
+        history.push("/");
+    }
     return(
-        <span>Profile</span>
+        <>
+            <span>Profile</span>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
     )
 }
