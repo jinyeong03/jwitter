5664b5944b88228c261c0f545a4cbb22c233c5e2
jinyeong03 <jinyeong2808@naver.com>
2023-02-23 15:28:20 +0900

    3.4

---
+++ src/components/Nweet.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function Nweet({ nweetObj, isOwner }){
+    return(
+        <div>
+            <h4>{nweetObj.text}</h4>
+            {
+                isOwner && (
+                    <>
+                        <button>Delete Nweet</button>
+                        <button>Edit Nweet</button>
+                    </>
+                )
+            }
+        </div>
+    );
+}
